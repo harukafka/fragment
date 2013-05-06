ffbe333d82c995fadefcda755f253928cd47a48f
harukafka <harukafka@gmail.com>
2013-05-06 13:05:45 +0900

    文字が上から降ってくる

--- 20130505/js/app.js
+++ 20130505/js/app.js
@@ -4,5 +4,13 @@ $(function(){
 			playoutAnimations: true
 		});	
 		$('#title-line1').lettering();
-		TweenMax.from($('#title-line1'), 1.5, {x:0, y:30 ,yoyo:true, repeat:-1, ease:Quad.easeIn});
+		//TweenMax.from($('#title-line1'), 1.5, {x:0, y:30 ,yoyo:true, repeat:3, ease:Quad.easeIn});
+		
+		TweenMax.from($("#title-line1"),0.3,{y:-300});
+		
+		$("#title-line1 span").each(function (i) {
+			var lag = i * 0.08;
+			TweenMax.from($(this), 0.5, {x:0, y:30, delay:lag, ease:Quad.easeOut});
+			i++;
+		});
 });
