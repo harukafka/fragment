00e0e89459e8ac97a0a3ba7daed392e51213cb7a
harukafka <harukafka@gmail.com>
2013-05-06 12:29:03 +0900

    文字列を浮遊させる

--- 20130505/js/app.js
+++ 20130505/js/app.js
@@ -4,4 +4,5 @@ $(function(){
 			playoutAnimations: true
 		});	
 		$('#title-line1').lettering();
+		TweenMax.from($('#title-line1'), 1.5, {x:0, y:30 ,yoyo:true, repeat:-1, ease:Quad.easeIn});
 });
